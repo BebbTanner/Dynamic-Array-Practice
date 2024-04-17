309c4c04d820a11eb22dfa1ec4e960db953387eb
Tanner Bebb <17tbebb@gmail.com>
2024-04-17 10:26:15 -0700

    Resturctured the dynamic array to allow the user to define both the size of the array, and the values stored in that array.

--- Dynamic Array Practice/Main.cpp
+++ Dynamic Array Practice/Main.cpp
@@ -8,37 +8,43 @@ With the template setup for the Dynamic array, Now I want to try and make someth
 #include <iostream>
 using namespace std;
 
-const int Capacity = 1000;
-
 int main() {
 
-	/*
-	This is a declaration of a pointer labeled dArray. 
-	dArray is then defined as a new integer array using the capacity variable defined above.
-	*/
+	int Capacity;
+
+/*
+This lets the user define capacity of the dynamic array.
+*/
+	cout << "How many values do you want to store in this array?: " << endl;
+	cin >> Capacity;
+
+/*
+This is a declaration of a pointer labeled dArray. 
+dArray is then defined as a new integer array using the capacity variable defined above.
+*/
 	int* dArray;
 	dArray = new int[Capacity];
 
-	/*
-	This is hard coding 4 values to the first 4 items in the dArray.
-	*/
-	dArray[0] = 0;
-	dArray[1] = 1;
-	dArray[2] = 2;
-	dArray[3] = 3;
 
-	cout << "Current array: " << endl;
 
-	/*
-	This is a for loop that is going to print out the 4 values defined above.
-	*/
-	for (int i = 0; i < 4; i++) {
-		cout << dArray[i] << endl;
+/*
+This is a for loop that will ask the user to input the values they want to store.
+*/
+	cout << "Please enter your " << Capacity << " values: " << endl;
+	for (int j = 0; j < Capacity; j++) {
+		cin >> dArray[j];
 	}
 
-	/*
-	This deletes the array pointer that was defined at the top of int main.
-	*/
+/*
+This is a for loop that will print out the values that are currently stored in dArray.
+*/
+	cout << "The values you entered are: " << endl;
+	for (int i = 0; i < Capacity;i++) {
+		cout << dArray[i] << endl;
+	}
+/*
+This deletes the array pointer that was defined at the top of int main.
+*/
 	delete[] dArray;
 
 	return 0;
